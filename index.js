23eee023659c720e760d7cdecdf225a7fb8debea
Muttaky <muttaky9@gmail.com>
2025-11-11 18:45:57 +0600

    vercelDone

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ async function run() {
       const result = await cropsColl.deleteOne(query);
       res.send(result);
     });
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
